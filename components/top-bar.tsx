63cddf164c61d701e41ae3020747c1fec68e11cb
zx3923 <seop3923@gmail.com>
2025-02-04 23:21:13 +0900

    link home

--- components/top-bar.tsx
+++ components/top-bar.tsx
@@ -41,7 +41,7 @@ export default function TopBar() {
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
                 <Link
-                  href="/"
+                  href="/home"
                   className="text-gray-300 hover:bg-neutral-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   홈
